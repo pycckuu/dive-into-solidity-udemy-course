40b5f357a88dd20090b6d2082733c41e3330feaa
pycckuu <is.markelov@gmail.com>
2022-08-01 22:54:01 +0200

    test: Lotery contract

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -9,38 +9,43 @@ contract Lottery {
     address[] public gameWinners;
     address public owner;
 
+    modifier onlyOwner() {
+        require(msg.sender == owner, "ONLY_OWNER");
+        _;
+    }
+
     // declaring the constructor
     constructor() {
-        // TODO: initialize the owner to the address that deploys the contract
+        owner = msg.sender;
     }
 
     // declaring the receive() function that is necessary to receive ETH
     receive() external payable {
-        // TODO: require each player to send exactly 0.1 ETH
-        // TODO: append the new player to the players array
+        require(msg.value == 0.1 ether, "to send exactly 0.1 ETH");
+        players.push(msg.sender);
     }
 
     // returning the contract's balance in wei
-    function getBalance() public view returns (uint256) {
-        // TODO: restrict this function so only the owner is allowed to call it
-        // TODO: return the balance of this address
+    function getBalance() public view onlyOwner returns (uint256) {
+        return address(this).balance;
     }
 
     // selecting the winner
-    function pickWinner() public {
-        // TODO: only the owner can pick a winner 
-        // TODO: owner can only pick a winner if there are at least 3 players in the lottery
+    function pickWinner() public onlyOwner {
+        require(players.length >= 3, "NOT_ENOUGH_PLAYERS");
 
         uint256 r = random();
-        address winner;
+        address payable winner;
 
-        // TODO: compute an unsafe random index of the array and assign it to the winner variable 
+        uint256 idx = r % players.length;
+        winner = payable(players[idx]);
 
-        // TODO: append the winner to the gameWinners array
+        gameWinners.push(winner);
 
-        // TODO: reset the lottery for the next round
+        delete players;
 
-        // TODO: transfer the entire contract's balance to the winner
+        (bool sent, ) = winner.call{value: address(this).balance}("");
+        require(sent, "Failed to send Ether");
     }
 
     // helper function that returns a big random integer
